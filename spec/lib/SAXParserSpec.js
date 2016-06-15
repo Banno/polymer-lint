1f77ab32b67a734425409ad03c9f15f3d2b2c984
Jordan Running <jordan.running@banno.com>
2016-06-15 11:33:10 -0500

    More tests and docs for SAXParser

--- spec/lib/SAXParserSpec.js
+++ spec/lib/SAXParserSpec.js
@@ -1,34 +1,42 @@
 const SAXParser = require('../../lib/SAXParser');
 
 describe('SAXParser', () => {
-  describe('event', () => {
+  function attributesContaining(attrsObj) {
+    return jasmine.arrayContaining(
+      Object.keys(attrsObj).map(name => ({ name, value: attrsObj[name] }))
+    );
+  }
+
+  describe('emits event', () => {
     const moduleId = 'foo-module';
+    const importHref = 'bar/baz-module.html';
 
     const component = `
+      <link rel="import" href="${importHref}"/>
       <dom-module id="${moduleId}">
-        <template>Hello</template>
+        <template>
+          Hello
+          <${moduleId} foo="bar">World</${moduleId}>
+        </template>
+
         <script>
           Polymer({ is: '${moduleId}' });
         </script>
       </dom-module>
     `;
 
-    let file, listener;
+    let file, listener, parser;
 
     beforeEach(() => {
       file = helpers.streamFromString(component);
       listener = jasmine.createSpy();
+      parser = new SAXParser();
     });
 
-    it('domModuleStartTag is emitted', (done) => {
-      const parser = new SAXParser();
-      parser.on('domModuleStartTag', listener);
-
-      parser.on('end', () => {
-        expect(listener).toHaveBeenCalled();
-
-        const args = listener.calls.argsFor(0);
-        expect(args[0]).toEqual(moduleId);
+    it('domModuleStartTag', (done) => {
+      parser.on('domModuleStartTag', (name, attrs) => {
+        expect(name).toEqual(moduleId);
+        expect(attrs).toEqual(attributesContaining({ id: moduleId }));
 
         done();
       });
@@ -36,20 +44,38 @@ describe('SAXParser', () => {
       file.pipe(parser);
     });
 
-    it('domModuleEndTag is emitted', (done) => {
-      const parser = new SAXParser();
-      parser.on('domModuleEndTag', listener);
+    it('domModuleEndTag', (done) => {
+      parser.on('domModuleEndTag', done);
+      file.pipe(parser);
+    });
 
-      parser.on('end', () => {
-        expect(listener).toHaveBeenCalled();
+    it('importTag', (done) => {
+      parser.on('importTag', (href) => {
+        expect(href).toEqual(importHref);
         done();
       });
 
       file.pipe(parser);
     });
 
-    it('importTag is emitted');
-    it('customElementStartTag is emitted');
-    it('customElementEndTag is emitted');
+    it('customElementStartTag', (done) => {
+      parser.on('customElementStartTag', (name, attrs, selfClosing) => {
+        expect(name).toEqual(moduleId);
+        expect(attrs).toEqual(attributesContaining({ foo: 'bar' }));
+        expect(selfClosing).toEqual(false);
+        done();
+      });
+
+      file.pipe(parser);
+    });
+
+    it('customElementEndTag', (done) => {
+      parser.on('customElementEndTag', (name) => {
+        expect(name).toEqual(moduleId);
+        done();
+      });
+
+      file.pipe(parser);
+    });
   });
 });
