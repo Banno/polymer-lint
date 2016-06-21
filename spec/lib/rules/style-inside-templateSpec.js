5302c1bc145d1de0032df8fc77e7d191efb88868
Jordan Running <jordan.running@banno.com>
2016-06-21 10:10:46 -0500

    Add style-inside-template rule

---
+++ spec/lib/rules/style-inside-templateSpec.js
@@ -0,0 +1,60 @@
+const { EventEmitter } = require('events');
+const styleInsideTemplate = require('../../../lib/rules/style-inside-template');
+
+describe('style-inside-template', () => {
+  const location = { line: 2, col: 3 };
+
+  let mockParser, onError;
+
+  beforeEach(() => {
+    mockParser = new EventEmitter;
+    onError = jasmine.createSpy('onError');
+    styleInsideTemplate({}, mockParser, onError);
+  });
+
+  describe('when <style> is inside <template>', () => {
+    beforeEach(() => {
+      mockParser.emit('startTag', 'template', {}, false, {});
+    });
+
+    it('does not call the onError callback', () => {
+      mockParser.emit('startTag', 'style', {}, false, {});
+      expect(onError).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('when <style> is before <template>', () => {
+    it('calls the onError callback with the expected arguments', () => {
+      mockParser.emit('startTag', 'style', {}, false, location);
+      mockParser.emit('startTag', 'template', {}, false, {});
+
+      expect(onError).toHaveBeenCalledWith({
+        message: '<style> tag outside of <template>', location,
+      });
+    });
+  });
+
+  describe('when <style> is after </template>', () => {
+    it('calls the onError callback with the expected arguments', () => {
+      mockParser.emit('startTag', 'template', {}, false, {});
+      mockParser.emit('endTag', 'template');
+      mockParser.emit('startTag', 'style', {}, false, location);
+
+      expect(onError).toHaveBeenCalledWith({
+        message: '<style> tag outside of <template>', location,
+      });
+    });
+  });
+
+  describe('when <template> is self-closing', () => {
+    it('calls the onError callback with the expected arguments', () => {
+      const selfClosing = true;
+      mockParser.emit('startTag', 'template', {}, selfClosing, {});
+      mockParser.emit('startTag', 'style', {}, false, location);
+
+      expect(onError).toHaveBeenCalledWith({
+        message: '<style> tag outside of <template>', location,
+      });
+    });
+  });
+});
