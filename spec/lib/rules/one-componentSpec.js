5776d68d92ebf0e5b493308118a2048f4d3d71cf
Jordan Running <jordan.running@banno.com>
2016-06-21 09:33:19 -0500

    Add one-component rule

---
+++ spec/lib/rules/one-componentSpec.js
@@ -0,0 +1,38 @@
+const { EventEmitter } = require('events');
+const oneComponent = require('../../../lib/rules/one-component');
+
+describe('one-component', () => {
+  let mockParser, onError;
+
+  beforeEach(() => {
+    mockParser = new EventEmitter;
+    onError = jasmine.createSpy('onError');
+
+    oneComponent({}, mockParser, onError);
+  });
+
+  describe('when one component is defined', () => {
+    beforeEach(() => {
+      mockParser.emit('domModuleStartTag', 'foo', {}, false, {});
+    });
+
+    it('does not call the onError callback', () => {
+      expect(onError).not.toHaveBeenCalled();
+    });
+
+    describe('and then a second component is defined', () => {
+      const location = { line: 6, col: 13 };
+
+      beforeEach(() => {
+        mockParser.emit('domModuleStartTag', 'bar', {}, false, location);
+      });
+
+      it('calls the onError callback with the expected arguments', () => {
+        expect(onError).toHaveBeenCalledWith({
+          message: 'More than one component defined: bar',
+          location,
+        });
+      });
+    });
+  });
+});
