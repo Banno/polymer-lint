41fb17120804db4852021e2aa06a0f354220eeac
Jordan Running <jordan.running@banno.com>
2016-06-21 17:30:28 -0500

    Docs improvements

--- bin/polymer-lint.js
+++ bin/polymer-lint.js
@@ -8,6 +8,6 @@ function handleError(err) {
 process.on('uncaughtException', handleError);
 process.on('unhandledRejection', handleError);
 
-Promise.resolve(CLI.execute(process.argv)).then(exitCode => {
+CLI.execute(process.argv).then(exitCode => {
   process.exitCode = exitCode;
 });
